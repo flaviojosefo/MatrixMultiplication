22226bf96ff790b38db641cfc00de565e7ef0e72
fs000 <flaviosantos002010@hotmail.com>
2022-11-02 18:04:13 +0000

    Add (classic) Matrix multiplication and transposing

--- MatrixMultiplication/Matrix.cs
+++ MatrixMultiplication/Matrix.cs
@@ -8,12 +8,14 @@ namespace MatrixMultiplication {
 
     internal struct Matrix {
 
-        private readonly float[] matrix;
+        private float[] matrix;
 
         public int Rows { get; }
         public int Cols { get; }
 
-        public int Length { get => Rows * Cols; }
+        public int Length { get => matrix.Length; }
+
+        public Matrix Transposed => Transpose(this);
 
         public Matrix(int rows, int cols) {
 
@@ -26,6 +28,53 @@ namespace MatrixMultiplication {
 
                 matrix[i] = i;
             }
+        }
+
+        public float this[int i] { get => matrix[i]; }
+
+        // Multiply 2 matrices and return the resulting matrix
+        public static Matrix MatMul(Matrix m1, Matrix m2) {
+
+            int m = m1.Rows;
+            int n = m2.Cols;
+            int l = m1.Cols;
+
+            float[] newMat = new float[m * n];
+
+            for (int i = 0; i < m; i++) {
+
+                for (int j = 0; j < n; j++) {
+
+                    float sum = 0;
+
+                    for (int k = 0; k < l; k++) {
+
+                        sum += m1[i * l + k] * m2[k * n + j];
+                    }
+
+                    newMat[i * n + j] = sum;
+                }
+            }
+
+            return new Matrix(m, n) { matrix = newMat };
+        }
+
+        public static Matrix Transpose(Matrix mat) {
+
+            int m = mat.Rows;
+            int n = mat.Cols;
+
+            float[] transposed = new float[mat.Length];
+
+            for (int i = 0; i < m; i++) {
+
+                for (int j = 0; j < n; j++) {
+
+                    transposed[j + i * n] = mat[i + j * m];
+                }
+            }
+
+            return new Matrix(n, m) { matrix = transposed };
         }
 
         // Return a readeable Matrix text representation
--- MatrixMultiplication/Program.cs
+++ MatrixMultiplication/Program.cs
@@ -4,9 +4,17 @@
 
         static void Main(string[] args) {
 
-            Matrix mat = new(3, 4);
+            Matrix mat1 = new(1, 4);
+            Matrix mat2 = new Matrix(4, 4).Transposed;
 
-            Console.WriteLine(mat);
+            Matrix mat3 = Matrix.MatMul(mat1, mat2);
+            Matrix mat4 = Matrix.Transpose(mat3);
+
+            Console.WriteLine(mat1);
+            Console.WriteLine(mat2);
+            Console.WriteLine(mat3);
+            Console.WriteLine(mat4);
+
             Console.ReadKey();
         }
     }
