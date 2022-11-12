8f61179c3701113027ee43332d6fd2963cf9da77
fs000 <flaviosantos002010@hotmail.com>
2022-11-12 04:16:45 +0000

    Add parallel multiplication to double index matrices

--- MatrixMultiplication/Matrix2D.cs
+++ MatrixMultiplication/Matrix2D.cs
@@ -11,23 +11,27 @@
 
         public float this[int i, int j] { get => matrix[i, j]; }
 
-        public static implicit operator Matrix2D(float[,] m) => new Matrix2D { matrix = m };
+        public static implicit operator Matrix2D(float[,] m) => new() { matrix = m };
 
-        public Matrix2D(int rows, int cols) {
+        public Matrix2D(int rows, int cols, bool fill = false, float? toFill = null) {
 
             matrix = new float[rows, cols];
 
-            int k = 0;
+            if (fill) {
 
-            for (int i = 0; i < rows; i++) {
+                int k = 0;
 
-                for (int j = 0; j < cols; j++) {
+                for (int i = 0; i < rows; i++) {
 
-                    matrix[i, j] = k++;
+                    for (int j = 0; j < cols; j++) {
+
+                        matrix[i, j] = toFill ?? k++;
+                    }
                 }
             }
         }
 
+        // Multiply 2 matrices and return the resulting matrix [classic]
         public static Matrix2D MatMul(Matrix2D m1, Matrix2D m2) {
 
             // Store Rows/Cols values of m1 and m2
@@ -64,6 +68,103 @@
 
             // Return the newly created matrix
             return newMatrix;
+        }
+
+        public static Matrix2D MatMulP(Matrix2D m1, Matrix2D m2, int tasks) {
+
+            // Store Rows/Cols values of m1 and m2
+            int m = m1.Rows;
+            int n = m2.Cols;
+            int l = m1.Cols;
+
+            // Verify if given matrices are valid
+            if (l != m2.Rows) {
+
+                Console.WriteLine("Classic Parallel Matrix Multiplication Failed: Cols of m1 must match Rows of m2!\n");
+                return new(m, n);
+            }
+
+            // The matrix to be returned
+            float[,] newMat = new float[m, n];
+
+            // Half the number of tasks acts as division for partition of m2
+            int halfTasks = tasks / 2;
+
+            // Matrix partitions boundaries
+            int halfRowsM1 = m / 2;
+            int divColsM2 = n / halfTasks;
+
+            // The tasks to initiate
+            Task[] partMuls = new Task[tasks];
+
+            // Loop through the number of tasks
+            for (int i = 0; i < tasks; i++) {
+
+                // Calculate "switches" that control m1 and m2 indexation
+                int m1Switch = -((i % halfTasks) - i) / halfTasks;
+                int m2Switch = i % halfTasks;
+
+                // Calculate on which row/col the multiplications should begin for the current partition
+                int m1Begin = halfRowsM1 * m1Switch;
+                int m2Begin = divColsM2 * m2Switch;
+
+                // Calculate on which row/col the multiplications should end for the current partition
+                int m1End = m1Begin + halfRowsM1;
+                int m2End = m2Begin + divColsM2;
+
+                // Inititate a task (queue a partition multiplication on the thread pool)
+                partMuls[i] = Task.Run(() => {
+
+                    // Standard (double index) matrix multiplication algorithm
+                    for (int i = m1Begin; i < m1End; i++) {
+
+                        for (int j = m2Begin; j < m2End; j++) {
+
+                            float sum = 0;
+
+                            for (int k = 0; k < l; k++) {
+
+                                sum += m1[i, k] * m2[k, j];
+                            }
+
+                            newMat[i, j] = sum;
+                        }
+                    }
+                });
+            }
+
+            // Wait for all tasks to finish
+            Task.WaitAll(partMuls);
+
+            //Parallel.For(0, tasks, (i) => {
+
+            //    int m1Switch = -((i % halfTasks) - i) / halfTasks;
+            //    int m2Switch = i % halfTasks;
+
+            //    int m1Begin = halfRowsM1 * m1Switch;
+            //    int m2Begin = divColsM2 * m2Switch;
+
+            //    int m1End = m1Begin + halfRowsM1;
+            //    int m2End = m2Begin + divColsM2;
+
+            //    for (int ix = m1Begin; ix < m1End; ix++) {
+
+            //        for (int j = m2Begin; j < m2End; j++) {
+
+            //            float sum = 0;
+
+            //            for (int k = 0; k < l; k++) {
+
+            //                sum += m1[ix * l + k] * m2[k * n + j];
+            //            }
+
+            //            newMat[ix * n + j] = sum;
+            //        }
+            //    }
+            //});
+
+            // Return the newly created matrix
+            return newMat;
         }
 
         // Return a readeable Matrix text representation
