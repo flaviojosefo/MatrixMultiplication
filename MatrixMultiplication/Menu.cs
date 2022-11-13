06c3c75483b1a2a11719d33a9991243ede3a2226
fs000 <flaviosantos002010@hotmail.com>
2022-11-13 00:11:04 +0000

    Display menus so the user can select which multiplication methods to use/compare

---
+++ MatrixMultiplication/Menu.cs
@@ -0,0 +1,240 @@
+﻿using System.Diagnostics;
+
+namespace MatrixMultiplication {
+
+    internal sealed class Menu {
+
+        private const string PROJECT_TITLE = "Matrix Multiplication";
+
+        // Const variables [CHANGE THESE]
+        private const int THREADS = 4;
+
+        private const int M1_ROWS = 2000;
+        private const int M1_COLS = 2000;
+        private const int M2_ROWS = 2000;
+        private const int M2_COLS = 2000;
+
+        private readonly Matrix m1;
+        private readonly Matrix m2;
+        private readonly Matrix m2T;
+        private Matrix result;
+
+        private readonly Matrix2D m1_2D;
+        private readonly Matrix2D m2_2D;
+        private Matrix2D result_2D;
+
+        private readonly string[] multMethods;
+
+        public Menu(int width, int height) {
+
+            // These methods stop the Console from flickering when clearing
+            // but are only supported on Windows OS
+            if (OperatingSystem.IsWindows()) {
+                Console.SetWindowSize(width, height);
+                Console.SetBufferSize(width, height);
+            }
+
+            // Hide the console's cursor
+            Console.CursorVisible = false;
+
+            // Set a project title
+            Console.Title = PROJECT_TITLE;
+
+            // The main menu's options
+            multMethods = new string[6] {
+                "Double Index [Single Thread]",
+                "Double Index [Multithread]",
+                "Linear Classic [Single Thread]",
+                "Linear Classic [Multithread]",
+                "Linear Transposed [Single Thread]",
+                "Linear Transposed [Multithread]",
+            };
+
+            // Build the 1D matrices
+            m1 = new(M1_ROWS, M1_COLS, true);
+            m2 = new(M2_ROWS, M2_COLS, true);
+            m2T = m2.Transposed;
+
+            // Build the 2D matrices
+            m1_2D = new(M1_ROWS, M1_COLS, true);
+            m2_2D = new(M2_ROWS, M2_COLS, true);
+        }
+
+        public void DisplayMultMethods(int cursor = 0, int menu = 0, int first = -1, int second = -1) {
+
+            // Clean the console
+            Console.Clear();
+
+            // Get the number of options
+            int nOptions = multMethods.Length;
+
+            if (menu == 0)
+                Console.WriteLine("SELECT THE FIRST MULTIPLICATION METHOD\n");
+            else
+                Console.WriteLine("SELECT THE SECOND MULTIPLICATION METHOD\n");
+
+            // The actual cursor (top) coordinates
+            int[] cursorCoord = new int[nOptions + 1];
+
+            // Print every option (+ its index)
+            for (int i = 0; i < nOptions; i++) {
+
+                cursorCoord[i] = Console.GetCursorPosition().Top;
+                Console.WriteLine($"  {i + 1}." + multMethods[i] + (i % 2 != 0 ? '\n' : ""));
+            }
+
+            cursorCoord[nOptions] = cursorCoord[nOptions - 1] + 2;
+
+            // Write the last option (of the current menu)
+            Console.WriteLine($"  {nOptions + 1}." + (menu == 0 ? "Exit" : "Back"));
+
+            // Print the 'cursor'
+            Console.SetCursorPosition(0, cursorCoord[cursor]);
+            Console.Write('►');
+
+            // Read user input
+            switch (Console.ReadKey().Key) {
+
+                case ConsoleKey.UpArrow:
+                    // Move the cursor up
+                    cursor = cursor > 0 ? cursor - 1 : nOptions;
+                    break;
+
+                case ConsoleKey.DownArrow:
+                    // Move the cursor down
+                    cursor = cursor < nOptions ? cursor + 1 : 0;
+                    break;
+
+                case ConsoleKey.Enter:
+                    // Select an option (menu)
+                    if (menu == 0)
+                        SelectMainOption(cursor, menu, cursor, second);
+                    else
+                        SelectMainOption(cursor, menu, first, cursor);
+                    break;
+            }
+
+            // Redraw the menu, if we moved the cursor
+            DisplayMultMethods(cursor, menu, first, second);
+        }
+
+        // Select an option on the main menu
+        private void SelectMainOption(int cursor, int menu = 0, int first = -1, int second = -1) {
+
+            // Check if the user selected (or not) the last option on the current menu
+            if (cursor < multMethods.Length) {
+
+                // Check on which menu the user is
+                if (menu == 0) {
+
+                    // Go to the second menu
+                    DisplayMultMethods(0, 1, first, second);
+
+                } else {
+
+                    // Run matrix multiplication and fo back to the "main" menu
+                    CompareMultSpeeds(new int[] {first, second});
+                    DisplayMultMethods(0, 0);
+                }
+
+            } else {
+
+                // Check on which menu the user is
+                if (menu == 0) {
+
+                    // Close the application
+                    Environment.Exit(0);
+
+                } else {
+
+                    // Go back to the "main" menu
+                    DisplayMultMethods(0, 0);
+                }
+            }
+        }
+
+        // Run 2 multiplications and write results on screen
+        private void CompareMultSpeeds(int[] indexes) {
+
+            // Clean the Console
+            Console.Clear();
+
+            // Return if both methods are the same
+            if (indexes[0] == indexes[1]) {
+
+                // Display message and await user input
+                Console.WriteLine("Selected methods are the same! Please choose different methods.");
+                Console.WriteLine("\n► 1.Back");
+                Console.ReadKey();
+
+                return;
+            }
+
+            // Current multiplication info
+            double[] operationsTime = new double[indexes.Length];
+
+            // Set a timer to evaluate performance
+            Stopwatch sw = new();
+            
+            for (int i = 0; i < indexes.Length; i++) {
+
+                Console.WriteLine($"---------- {multMethods[indexes[i]]} ----------\n");
+
+                // Restart the timer
+                sw.Restart();
+
+                // Multiply matrices by their respective allocated index
+                switch (indexes[i]) {
+
+                    case 0:
+                        result_2D = Matrix2D.MatMul(m1_2D, m2_2D);
+                        break;
+
+                    case 1:
+                        result_2D = Matrix2D.MatMulP(m1_2D, m2_2D, THREADS);
+                        break;
+
+                    case 2:
+                        result = Matrix.MatMulC(m1, m2);
+                        break;
+
+                    case 3:
+                        result = Matrix.MatMulPC(m1, m2, THREADS);
+                        break;
+
+                    case 4:
+                        result = Matrix.MatMulT(m1, m2T);
+                        break;
+
+                    case 5:
+                        result = Matrix.MatMulPT(m1, m2T, THREADS);
+                        break;
+                }
+
+                operationsTime[i] = sw.Elapsed.TotalMilliseconds;
+
+                // Print the resulting matrix
+                if (indexes[i] < 2)
+                    Console.WriteLine(result_2D);
+                else
+                    Console.WriteLine(result);
+
+                Console.WriteLine($"Time ({multMethods[indexes[i]]}): {operationsTime[i]:0.000} ms\n");
+                Console.WriteLine($"----------------------------------------------------\n");
+            }
+
+            // Stop the timer
+            sw.Stop();
+
+            // Show which method was faster and the correspondant performance difference
+            if (operationsTime[0] > operationsTime[1])
+                Console.WriteLine($"{multMethods[indexes[1]]} was {operationsTime[0] / operationsTime[1]:0.00}x faster than {multMethods[indexes[0]]}!");
+            else
+                Console.WriteLine($"{multMethods[indexes[0]]} was {operationsTime[1] / operationsTime[0]:0.00}x faster than {multMethods[indexes[1]]}!");
+
+            // Write and wait for user input
+            Console.WriteLine("\n► 1.Back");
+            Console.ReadKey();
+        }
+    }
+}
--- MatrixMultiplication/Program.cs
+++ MatrixMultiplication/Program.cs
@@ -1,94 +1,12 @@
-﻿using System;
-using System.Diagnostics;
-
-namespace MatrixMultiplication {
+﻿namespace MatrixMultiplication {
 
     internal class Program {
 
         static void Main(string[] args) {
 
-            int m = 2000;
-            int n = 2000;
-            int l = 2000;
-
-            Matrix mat1 = new Matrix(m, n, true);
-            Matrix mat2 = new Matrix(n, l, true);
-
-            Matrix mat2T = mat2.Transposed;
-
-            //Matrix2D mat5 = new Matrix2D(m, n);
-            //Matrix2D mat6 = new Matrix2D(n, l);
-
-            Console.WriteLine("Calculating...\n");
-
-            Stopwatch sw = Stopwatch.StartNew();
-
-            //Matrix2D mat7 = Matrix2D.MatMul(mat5, mat6);
-            //double classicDouble = sw.Elapsed.TotalMilliseconds;
-
-            //sw.Restart();
-
-            //Matrix mat3 = Matrix.MatMulC(mat1, mat2);
-            //double classicLinear = sw.Elapsed.TotalMilliseconds;
-
-            //sw.Restart();
-
-            //Matrix mat4 = Matrix.MatMulT(mat1, mat2T);
-            //double transposed = sw.Elapsed.TotalMilliseconds;
-
-            sw.Restart();
-
-            Matrix matPC = Matrix.MatMulPC(mat1, mat2, 8);
-            double linearParallel = sw.Elapsed.TotalMilliseconds;
-
-            sw.Restart();
-
-            Matrix matPT = Matrix.MatMulPT(mat1, mat2T, 8);
-            double transposedParallel = sw.Elapsed.TotalMilliseconds;
-
-            sw.Stop();
-
-            //Console.WriteLine(mat1);
-            //Console.WriteLine(mat2);
-
-            //Console.Write(mat7);
-            //Console.WriteLine($"Time (Classic - Double Index): {classicDouble:0.000} ms\n");
-
-            //Console.Write(mat3);
-            //Console.WriteLine($"Time (Classic - Linear): {classicLinear:0.000} ms\n");
-
-            //Console.Write(mat4);
-            //Console.WriteLine($"Time (Transposed): {transposed:0.000} ms\n");
-
-            Console.Write(matPC);
-            Console.WriteLine($"Time (Linear Parallel): {linearParallel:0.000} ms\n");
-
-            Console.Write(matPT);
-            Console.WriteLine($"Time (Transposed Parallel): {transposedParallel:0.000} ms\n");
-
-            //string dlSpeed = classicLinear > classicDouble ?
-            //    $"    Double to Linear: Slow down of {classicLinear / classicDouble:0.00}x\n" :
-            //    $"    Double to Linear: Speed up of {classicDouble / classicLinear:0.00}x\n";
-
-            //string dtSpeed = transposed > classicDouble ?
-            //    $"Double to Transposed: Slow down of {transposed / classicDouble:0.00}x\n" :
-            //    $"Double to Transposed: Speed up of {classicDouble / transposed:0.00}x\n";
-
-            //string ltSpeed = transposed > classicLinear ?
-            //    $"Linear to Transposed: Slow down of {transposed / classicLinear:0.00}x\n" :
-            //    $"Linear to Transposed: Speed up of {classicLinear / transposed:0.00}x\n";
-
-            //string lpSpeed = linearParallel > classicLinear ?
-            //    $"Linear to Parallel: Slow down of {linearParallel / classicLinear:0.00}x\n" :
-            //    $"Linear to Parallel: Speed up of {classicLinear / linearParallel:0.00}x\n";
-
-            string tpSpeed = transposedParallel > linearParallel ?
-                $"Classic Parallel to Transposed Parallel: Slow down of {transposedParallel / linearParallel:0.00}x\n" :
-                $"Classic Parallel to Transposed Parallel: Speed up of {linearParallel / transposedParallel:0.00}x\n";
-
-            Console.WriteLine(/*dlSpeed + dtSpeed + ltSpeed + lpSpeed + */tpSpeed);
-
-            Console.ReadKey();
+            // Start a new menu and display it
+            Menu menu = new(120, 40);
+            menu.DisplayMultMethods();
         }
     }
 }
